79f9710ead2223972f0df5385f68e08647ff8ecc
mlinaresweb <ghaedesigns@gmail.com>
2022-08-27 20:05:41 +0200

    Calculadora
    Simple calculator

---
+++ script.js
@@ -0,0 +1,28 @@
+var operaciones = function (e) {
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    var result = document.getElementById("resultado");
+    var operacion = e.target.id;
+    
+
+    if (operacion == "suma") {
+        var resultado = numero1 + numero2;
+    }
+    else if (operacion == "resta") {
+        var resultado = numero1 - numero2;
+    }
+    else if (operacion == "multiplica") {
+        var resultado = numero1 * numero2;
+    }
+    else if (operacion == "divide") {
+        var resultado = numero1 / numero2;
+    }
+
+    result.innerHTML = resultado;
+    
+}
+
+
+//Obtenemos el evento click en cada boton
+var botones = document.getElementById("btns");
+botones.addEventListener("click", operaciones);
